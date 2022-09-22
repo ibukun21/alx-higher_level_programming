70a6b5f08c2bf5101509027fd6f913b780c64f39
ibukun21 <83639540+ibukun21@users.noreply.github.com>
2022-09-22 23:55:25 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(c, chr(c - 33)), end="")
+for l in range(123, 97) and u in range(90, 65):
+    print("{}{}".format(l, u), end="")
