3c2c2ce4956c7865559cac1cffdb8ad0b318a17c
ibukun21 <83639540+ibukun21@users.noreply.github.com>
2022-09-28 00:05:16 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,86 +1,72 @@
 #include "lists.h"
 
 /**
- * is_palindrome - chexk if a list is palindrome
- * Description:
- * The approach uses the fact that a list
- * - is palindrom if:
- *   its first half is thesame as the reverse
- *   of its last half
- * - else its not a palindrom
- * The middle element is found using the
- * tortoise hare algorithm
- * In the case where the list has odd length
- * the middle element is disregarded
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * @list: pointer to list
- * Return: 1 if true 0 otherwise
+ * Return: pointer to the first node in the new list
  */
-
-int is_palindrome(listint_t **list)
+void reverse_listint(listint_t **head)
 {
-	int result = 1;
-	listint_t *fast, *slow, *h1, *h2, *tmp;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-	if (!list)
-		return (0);
-
-	/* if list length is less than 2 */
-	if (!(*list) || !((*list)->next))
-		return (1);
-
-	/* find the middle node */
-	slow = fast = *list;
-	while (fast && fast->next)
+	while (current)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-	if (fast)
-		slow = slow->next;
-
-	/* reverse the last half */
-	h2 = reverse_listint(&slow);
-	tmp = h2;
-
-	/* compare the two halves */
-	h1 = *list;
-	while (h2)
-	{
-		if (h2->n != h1->n)
-		{
-			result = 0;
-			break;
-		}
-		h1 = h1->next;
-		h2 = h2->next;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
 
-	/* rever the half back to original */
-	reverse_listint(&tmp);
-	return (result);
+	*head = prev;
 }
 
 /**
- * reverse_listint - reverses a linked list
- * @h: head of the list
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
  *
- * Return: pointer to the reversed
+ * Return: 1 if it is, 0 if not
  */
-listint_t *reverse_listint(listint_t **h)
+int is_palindrome(listint_t **head)
 {
-	listint_t *tmp2, *tmp1;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (!h || !(*h))
-		return (NULL);
-	tmp1 = (*h)->next;
-	(*h)->next = NULL;
-	while (tmp1)
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (1)
 	{
-		tmp2 = *h;
-		*h = tmp1;
-		tmp1 = (*h)->next;
-		(*h)->next = tmp2;
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	return (*h);
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+	else
+		return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
