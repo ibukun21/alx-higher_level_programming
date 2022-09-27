67a29b7e93779c1e5ceab5f9e6a272f463357171
ibukun21 <omodaraibukun211@gmail.com>
2022-09-28 00:23:16 +0100

    updating lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,7 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int j);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
