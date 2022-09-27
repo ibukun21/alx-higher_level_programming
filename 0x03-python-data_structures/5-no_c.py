b196a049bc18ba116a30a5b5f7773648c063c7df
ibukun21 <omodaraibukun211@gmail.com>
2022-09-27 04:20:39 +0100

    creating 4-new_in_list.py and 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ''
+    for j in range(len(my_string)):
+        if my_string[i].lower() != 'c':
+            new_string += my_string[j]
+    return new_string
