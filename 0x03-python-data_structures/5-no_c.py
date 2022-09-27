387b476ffca0a6db5d3271fab2c4bd0b193e47e0
ibukun21 <omodaraibukun211@gmail.com>
2022-09-27 04:30:20 +0100

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     new_string = ''
     for j in range(len(my_string)):
-        if my_string[i].lower() != 'c':
+        if my_string[j].lower() != 'c':
             new_string += my_string[j]
     return new_string
