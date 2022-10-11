a93bd23dfad1cec31b7fae6918ff1a5f57d582f4
ibukun21 <omodaraibukun211@gmail.com>
2022-10-11 23:33:39 +0100

    updating 0x00-python-hello_world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for loop in linked list
+ * @list : linked list head
+ *
+ * Return: 0 OR 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr = list, *str = list;
+
+	while (ptr && str && str->next)
+	{
+		ptr = ptr->next;
+		str = str->next->next;
+		if (str == ptr)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{:s}\n{:.9}".format((str * 3), str))
+print(f"{3 * str}\n{str[:9]}")
