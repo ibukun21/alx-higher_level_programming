22932b526d954995b44582a113751d47de703743
ibukun21 <83639540+ibukun21@users.noreply.github.com>
2022-10-11 21:17:18 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,55 +1,66 @@
 #!/usr/bin/python3
-class Square:
-    """Simple square class with his size as a field"""
 
+"""
+A script that creates a class and validates its given attribute
+Attributes:
+    Square (class): with a valid attribute 'size'
+Todo:
+    * Private instance attribute: size
+    * property setter def 'size(self, value)' to set it
+    * size must be an integer, otherwise raise a 'TypeError' exception with
+    the message size must be an integer
+    * if size is less than 0, raise a 'ValueError' exception with the message
+    size must be >= 0
+    * Public instance method: 'def area(self)' that returns the current
+    square area
+    * Public instance method: def my_print(self): that prints in stdout the
+    square with the character #
+"""
+
+
+class Square:
+    """
+        Atrributes:
+            __init__ (function): assign and validate attributes
+            area (function): calculate area of square
+    """
     def __init__(self, size=0):
-        """ Instance the class Square
-            Arguments:
-                @size: the size of every side of the Square,
-                        it must be a positive integer value"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        """
+        Args:
+            self (Square): object
+            size (int): size of square
+        """
+        self.__size = size
 
     @property
     def size(self):
-        """ Getter for the field size as a property
-            Return:
-                    Value of size"""
-        return (self.__size)
+        """size getter"""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """ Setter for the field size as a property.
-            Arguments:
-                @value: the value of size
-                        that must be a positive integer value."""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        """size setter"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
-        """ Compute the area of a square
-            with the formula:
-                                area = @size ^ 2 = @size * @size
-            Return:
-                    Power of the Square size to 2
-                    or size multiplicated by size."""
-        return (self.__size ** 2)
+        """
+        Args:
+            self (Square): instance of class
+        Return:
+            area (int)
+        """
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """ Prints a square using the character # in the standard output
-            or a blank line if @size is 0"""
-        if not self.size:
-            print()
-        else:
-            for row in range(self.__size):
-                for column in range(self.__size):
-                    print("#", end="")
+        """Print the Square to stdout using '#'"""
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
                 print()
+        else:
+            print()
