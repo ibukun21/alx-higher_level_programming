f0074071db7ed43eb2c83f69ff99e1aee38d5bfa
ibukun21 <omodaraibukun211@gmail.com>
2022-10-18 16:27:35 +0100

    updating 0x09-python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-A module that showcases locked class
-instance attribute
+LockedClass
 """
 
-class LockedClass:
-    """
-    Prevent dynamic creation of attributes by new instances
-    """
-    __slots__ = ('first_name',)
 
-    def __init__(self):
-        pass
+class LockedClass:
+    """ No class or object attributes, can't set
+        Except for first_name
+    """
+    def __setattr__(self, attribute, value):
+        if attribute == "first_name":
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
