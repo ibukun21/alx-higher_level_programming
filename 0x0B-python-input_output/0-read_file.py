23c1f6e581bac26802da128173ead440508531ac
ibukun21 <omodaraibukun211@gmail.com>
2022-10-27 00:37:21 +0100

    creating 0x0B-python-input_output project

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/Python3
+
+"""
+A module: Defines a function that reads from a file
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints to stdout
+    Args:
+        filename (str): name of filename
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end="")
